6c82f391e10339f9869dc8112ea2705b18fa3d3e
ocampo13 <cristian.ocampob@campusucc.edu.co>
2025-02-14 01:23:01 +0000

    Ejercicios Clase2

---
+++ Clase2/variables.java
@@ -0,0 +1,21 @@
+public class variables
+
+{
+
+    // Metodo Main
+    public static void main(String[] args) {
+
+        // Tipos de Datos
+        // Enteros int, long
+        int edad = 25;
+        // Reales double, float
+        double altura = 1.75;
+        // Cadenas
+        String nombre = "Juan";
+        // Logicos
+        boolean esEstudiante = true; // False
+        System.out.println(
+                "Nombre: " + nombre + ", Edad: " + edad + ", Altura: " + altura + ", Estudiante: " + esEstudiante);
+
+    }
+}
